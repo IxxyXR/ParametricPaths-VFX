c07fed3d5508161e5cd1c6fe329706adb1b14a06
Andy Baker <andy@andybak.net>
2021-08-21 18:18:26 +0100

    Go a bit mental with refraction

---
+++ Assets/Parametric Paths VFX/Scripts/RotateGameobject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class RotateGameobject : MonoBehaviour
+{
+    public Vector3 angles = Vector3.zero;
+    
+    void Update()
+    {
+        var rot = transform.localRotation;
+        rot *= Quaternion.Euler(angles);
+        transform.localRotation = rot;
+    }
+}
